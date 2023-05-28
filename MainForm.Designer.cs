81e35c393a9ec209108d66435cc84903996a532e
Andrey Ivanov <andyiva00@gmail.com>
2023-05-28 18:14:14 +0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LabCalculatorEngineering
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelLeft = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelRight = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBoxStylish = new System.Windows.Forms.CheckBox();
+            this.labelExpression = new System.Windows.Forms.Label();
+            this.textBoxOperand = new System.Windows.Forms.TextBox();
+            this.listBoxOperationHistory = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.tableLayoutPanelLeft.SuspendLayout();
+            this.tableLayoutPanelRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelMain.ColumnCount = 2;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelLeft, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelRight, 1, 0);
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 1;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // tableLayoutPanelLeft
+            // 
+            this.tableLayoutPanelLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelLeft.ColumnCount = 1;
+            this.tableLayoutPanelLeft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelLeft.Controls.Add(this.labelExpression, 0, 0);
+            this.tableLayoutPanelLeft.Controls.Add(this.textBoxOperand, 0, 1);
+            this.tableLayoutPanelLeft.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelLeft.Name = "tableLayoutPanelLeft";
+            this.tableLayoutPanelLeft.RowCount = 3;
+            this.tableLayoutPanelLeft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanelLeft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelLeft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanelLeft.Size = new System.Drawing.Size(459, 420);
+            this.tableLayoutPanelLeft.TabIndex = 0;
+            // 
+            // tableLayoutPanelRight
+            // 
+            this.tableLayoutPanelRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelRight.ColumnCount = 1;
+            this.tableLayoutPanelRight.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelRight.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelRight.Controls.Add(this.checkBoxStylish, 0, 0);
+            this.tableLayoutPanelRight.Controls.Add(this.listBoxOperationHistory, 0, 1);
+            this.tableLayoutPanelRight.Location = new System.Drawing.Point(468, 3);
+            this.tableLayoutPanelRight.Name = "tableLayoutPanelRight";
+            this.tableLayoutPanelRight.RowCount = 2;
+            this.tableLayoutPanelRight.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanelRight.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelRight.Size = new System.Drawing.Size(305, 420);
+            this.tableLayoutPanelRight.TabIndex = 1;
+            // 
+            // checkBoxStylish
+            // 
+            this.checkBoxStylish.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxStylish.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBoxStylish.AutoSize = true;
+            this.checkBoxStylish.Location = new System.Drawing.Point(246, 3);
+            this.checkBoxStylish.Name = "checkBoxStylish";
+            this.checkBoxStylish.Size = new System.Drawing.Size(56, 26);
+            this.checkBoxStylish.TabIndex = 0;
+            this.checkBoxStylish.Text = "Stylish";
+            this.checkBoxStylish.UseVisualStyleBackColor = true;
+            // 
+            // labelExpression
+            // 
+            this.labelExpression.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelExpression.AutoSize = true;
+            this.labelExpression.Location = new System.Drawing.Point(3, 0);
+            this.labelExpression.Name = "labelExpression";
+            this.labelExpression.Size = new System.Drawing.Size(453, 21);
+            this.labelExpression.TabIndex = 0;
+            this.labelExpression.Text = "Expression";
+            // 
+            // textBoxOperand
+            // 
+            this.textBoxOperand.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOperand.Location = new System.Drawing.Point(3, 24);
+            this.textBoxOperand.Multiline = true;
+            this.textBoxOperand.Name = "textBoxOperand";
+            this.textBoxOperand.ReadOnly = true;
+            this.textBoxOperand.Size = new System.Drawing.Size(453, 36);
+            this.textBoxOperand.TabIndex = 1;
+            this.textBoxOperand.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // listBoxOperationHistory
+            // 
+            this.listBoxOperationHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxOperationHistory.FormattingEnabled = true;
+            this.listBoxOperationHistory.ItemHeight = 16;
+            this.listBoxOperationHistory.Location = new System.Drawing.Point(3, 43);
+            this.listBoxOperationHistory.Name = "listBoxOperationHistory";
+            this.listBoxOperationHistory.Size = new System.Drawing.Size(299, 372);
+            this.listBoxOperationHistory.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Name = "MainForm";
+            this.Text = "Engineering calculator";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.tableLayoutPanelLeft.ResumeLayout(false);
+            this.tableLayoutPanelLeft.PerformLayout();
+            this.tableLayoutPanelRight.ResumeLayout(false);
+            this.tableLayoutPanelRight.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelLeft;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelRight;
+        private System.Windows.Forms.CheckBox checkBoxStylish;
+        private System.Windows.Forms.Label labelExpression;
+        private System.Windows.Forms.TextBox textBoxOperand;
+        private System.Windows.Forms.ListBox listBoxOperationHistory;
+    }
+}
+
